fe074a609499c5d5442ce3871f29aa656e047530
Casey DeMattina <cdemattina@gmail.com>
2023-09-18 22:16:57 -0400

    mostly all in responsive styles

--- src/components/Service.js
+++ src/components/Service.js
@@ -4,15 +4,17 @@ import Card from "./UI/Card";
 import './styles/Service.css';
 function Service(props) {
     const id = "service-" + props.id;
-    return (            
-        <Card id={id} >
-            <div className="row service" >
-                <h2 className="service-title">{props.title}</h2>
-                <div className="column services__left">
-                    <Image src={props.image} alt="{props.title} image"/>
+    return (
+        <Card id={id} >           
+            <div className="row">
+                <div className="col-sm">
+                    {props.title}
                 </div>
-                <div className="column services__right">
-                    {props.text}
+                <div className="col-sm">
+                    <Image className="service_image" src={props.image} alt="{props.title} image" />
+                </div>
+                <div className="col-sm">
+                {props.text}
                 </div>
             </div>
         </Card>
--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,7 +1,7 @@
 import '../styles/Card.css';
 function Card(props) {
 
-  const classes = 'card ' + (props.className ? props.className : '');
+  const classes = 'container card ' + (props.className ? props.className : '');
   const id = (props.id ? props.id : '');
   return <div className={classes} id={id}>{props.children}</div>;
 }
