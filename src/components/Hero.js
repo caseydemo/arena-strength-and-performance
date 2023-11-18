31f905d5edebe04c6e944a3acf2eee76baa416dd
Casey DeMattina <cdemattina@gmail.com>
2023-11-18 10:56:11 -0500

    bunch of style updates per jesse, more to come

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,10 +1,9 @@
 import './styles/Hero.css';
 function Hero() {
     return (
-
-      <div className="hero-image">
+      <div className="hero-image" id="hero-image">
         <div className="hero-text">
-          <h1>Arena Strength and Performance</h1>
+          <h1 className="hero-title">Arena Strength & Performance</h1>
           <p>Lexington KY's premiere weight training gym</p>          
         </div>
       </div>       
